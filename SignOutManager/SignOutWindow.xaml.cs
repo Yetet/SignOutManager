4da4833799401f138bd7e606176492e3f40dc19f
GABRIEL HOOKS <19hookgabr@gslions.net>
2018-03-22 13:00:05 -0400

    Initial commit

--- SignOutManager/SignOutWindow.xaml.cs
+++ SignOutManager/SignOutWindow.xaml.cs
@@ -120,6 +120,8 @@ namespace SignOutManager
                 writer.Serialize(file, _studentLog);
                 file.Close();
 
+                PrintXML(_xmlPath);
+
                 Console.WriteLine("Successfully created XML log.");
                 MessageBox.Show("Successfully printed log.");
             }
@@ -147,6 +149,47 @@ namespace SignOutManager
                 Console.WriteLine(e.Message);
                 MessageBox.Show("Could not delete XML log. " + e.Message);
             }
+        }
+
+        public static String PrintXML(String XML)
+        {
+            String Result = "";
+
+            MemoryStream mStream = new MemoryStream();
+            XmlTextWriter writer = new XmlTextWriter(mStream, Encoding.Unicode);
+            XmlDocument document = new XmlDocument();
+
+            try
+            {
+                // Load the XmlDocument with the XML.
+                document.LoadXml(XML);
+
+                writer.Formatting = Formatting.Indented;
+
+                // Write the XML into a formatting XmlTextWriter
+                document.WriteContentTo(writer);
+                writer.Flush();
+                mStream.Flush();
+
+                // Have to rewind the MemoryStream in order to read
+                // its contents.
+                mStream.Position = 0;
+
+                // Read MemoryStream contents into a StreamReader.
+                StreamReader sReader = new StreamReader(mStream);
+
+                // Extract the text from the StreamReader.
+                String FormattedXML = sReader.ReadToEnd();
+
+                Result = FormattedXML;
+            }
+            catch (XmlException)
+            {
+            }
+
+            mStream.Close();
+
+            return Result;
         }
     }
 }
